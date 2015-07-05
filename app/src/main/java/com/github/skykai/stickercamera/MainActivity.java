3b06b75638878a7e9a90006c41f0c60f132c115d
skykai521 <1132234509@qq.com>
2015-07-05 23:05:03 +0800

    add Gpu-Image library

--- app/src/main/java/com/github/skykai/stickercamera/MainActivity.java
+++ app/src/main/java/com/github/skykai/stickercamera/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
